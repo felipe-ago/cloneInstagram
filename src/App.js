13da820d4a7a7824b752860b2d243a0a4d035f7d
Felipe Oliveira <lipe111094@hotmail.com>
2023-02-10 10:06:15 -0300

    Logo and login field

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { db } from "./firebase.js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  useEffect(() => {
-    console.log(db);
-  }, []);
-  return (
-  <div className="App">
+  const [user, setUser] = useState(null);
 
-  </div>
+  useEffect(() => {}, []);
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="center">
+          <div className="header_logo">
+            <a href="https://www.instagram.com/">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png" />
+            </a>
+          </div>
+          {
+            (user)?
+            <div>Testando</div>
+            :
+            <div className="header_loginForm">
+            <form>
+              <input type="text" placeholder="Login..." />
+              <input type="password" placeholder="Senha..." />
+              <input type="submit" name="acao" value="Logar" />
+            </form>
+          </div>
+          }
+
+        </div>
+      </div>
+    </div>
   );
 }
 
